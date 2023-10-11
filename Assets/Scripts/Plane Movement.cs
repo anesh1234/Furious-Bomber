c7b7ee0feb2e4532f94e416e8ba8ea49163ea6b5
Anders M <anders.minde@gmail.com>
2023-10-11 23:20:25 +0200

    Added VFX

--- Assets/Scripts/Plane Movement.cs
+++ Assets/Scripts/Plane Movement.cs
@@ -5,29 +5,43 @@ using UnityEngine;
 public class Character : MonoBehaviour
 {
 
-    public float speed = 10.0f;
+    public float forwardVelocity = 10.0f;
 
     // Start is called before the first frame update
     void Start()
     {
+
     }
 
     // Update is called once per frame
     void Update()
     {
+        float rotation = Input.GetAxis("Vertical") * 0.010f;
+        rotation *= Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            transform.Rotate(rotation, 0, 0);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            transform.Rotate(-rotation, 0, 0);
+        }
+
+        //transform.Rotate(rotationLeft, 0, 0);
+        //transform.Rotate(rotationRight, 0, 0);
+
         // Get the horizontal and vertical axis.
         // By default they are mapped to the arrow keys.
         // The value is in the range -1 to 1
-
         // Flyet har fucka koordinatsystem, så flyets y-akse = unity's x-akse
-      
-        float translationY = Input.GetAxis("Horizontal") * speed;
-        float translationX = speed;
+
+        float translationY = Input.GetAxis("Horizontal") * forwardVelocity;
+        float translationX = forwardVelocity;
 
         // Make it move 10 meters per second instead of 10 meters per frame...
         translationY *= Time.deltaTime;
         translationX *= Time.deltaTime;
-
         // Move translation along the object's x-axis
         transform.Translate(0, -translationY, 0);
 
