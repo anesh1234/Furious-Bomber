db9dbb5d6fd0e19424aa691a8fbf0b8fbd1543fd
Anders M <anders.minde@gmail.com>
2023-12-04 21:13:26 +0100

    scene loading enabled, keep struggling with the ui

--- Assets/Scripts/SubMenuManager.cs
+++ Assets/Scripts/SubMenuManager.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SubMenuManager : MonoBehaviour
 {
+    public GameObject LoadingScreen;
+
 
     // Start is called before the first frame update
     void Start()
@@ -15,5 +18,11 @@ public class SubMenuManager : MonoBehaviour
     public void OnClickBack()
     {
         this.gameObject.SetActive(false);
+    }
+
+
+    public void LoadScene(int sceneId)
+    {
+        AsyncOperation sceneLoad = SceneManager.LoadSceneAsync(sceneId);
     }
 }
