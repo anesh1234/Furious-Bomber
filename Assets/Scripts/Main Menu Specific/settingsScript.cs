29eedb88d4817f22ecd0ca6c1ab3791712c24e98
Sanityyy <stiannordholm@gmail.com>
2023-12-14 18:32:24 +0100

    Added functionality to settings menu

--- Assets/Scripts/Main Menu Specific/settingsScript.cs
+++ Assets/Scripts/Main Menu Specific/settingsScript.cs
@@ -2,9 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+using TMPro;
+using System.Linq;
 
 public class settingsScript : MonoBehaviour
 {
+    private Resolution[] resolutions;
+    public TMPro.TMP_Dropdown resolutionDropdown;
+
+    private void Start()
+    {
+        resolutions = Screen.resolutions.Select(resolution => new Resolution { width = resolution.width, height = resolution.height }).Distinct().ToArray();
+        resolutionDropdown.ClearOptions();
+
+        List<string> options = new List<string>();
+
+        int currentResolutionIndex = 0;
+        for (int i = 0; i < resolutions.Length; i++)
+        {
+
+            string option = resolutions[i].width + " x " + resolutions[i].height;
+            options.Add(option);
+
+
+
+
+            if (resolutions[i].width == Screen.currentResolution.width &&
+                resolutions[i].height == Screen.currentResolution.height)
+            {
+                currentResolutionIndex = i;
+            }
+        }
+
+        resolutionDropdown.AddOptions(options);
+        resolutionDropdown.value = currentResolutionIndex;
+    }
+
+    public void setResolution(int resolutionIndex)
+    {
+        Resolution resolution = resolutions[resolutionIndex];
+        Screen.SetResolution(resolution.width, resolution.height, Screen.fullScreen);
+    }
 
     public AudioMixer audioMixer;
 
@@ -12,5 +51,15 @@ public class settingsScript : MonoBehaviour
     {
         Debug.Log(volume);
         audioMixer.SetFloat("volume", Mathf.Log10(volume) * 20);
+    }
+
+    public void setQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+
+    public void setFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
     }
 }
